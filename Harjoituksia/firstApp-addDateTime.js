9de76ae58d2f7211174108f5793056287c7f676d
Zhao <61022230+zircomars@users.noreply.github.com>
2026-01-05 17:51:37 +0200

    Create firstApp-addDateTime.js

---
+++ Harjoituksia/firstApp-addDateTime.js
@@ -0,0 +1,54 @@
+// current timestamp in milliseconds
+let date_ob = new Date();
+
+// tän hetkisen pv.kk.vvvv ja aika
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+
+//päiväys asentaminen erikseen joko pv.kk tai kk.pv (maku asia) ja aika
+console.log("Tänään on: " + date + "-" + month + "-" + year + " \n" + hours + ":" + minutes + ":" + seconds);
+
+
+var http = require('http');
+//var dt = require('index');
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("asdfasdf \n" );
+    
+    response.write("Today is: " + date + "-" + month + "-" + year);
+    //Tallentaa joku tämmöisen json database tietokanta ja lukaisee minkä file paketin
+    //res.write("Today is... " + dt.myDateTime());
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
+/*
+var http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(250, {'Content-Type': 'text/plain'});
+    response.write('asdf');
+    response.end();
+}
+
+
+http.createServer(onRequest).listen(8000);
+
+//////////////////////////////////////////////
+/// w3schools
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+
+*/
